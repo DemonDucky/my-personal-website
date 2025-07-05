a296fd64840b812c991e25028037a384b666c034
demonducky <cuti1mat@gmail.com>
2025-07-06 05:00:03 +0700

    fix: some small visual

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+	const currentYear = new Date().getFullYear();
 </script>
 
 <footer>
 	<div class="text-muted-foreground container mx-auto px-6 py-8 text-center text-xs">
-		<p>© 2025 Luong Tuan Anh. All rights reserved.</p>
+		<p>© {currentYear} Luong Tuan Anh. All rights reserved.</p>
 	</div>
 </footer>
